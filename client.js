612470626236e9c47e0a8a606129ce2fcf52556e
jeff9014223 <9014223@student.zadkine.nl>
2023-10-05 09:12:57 +0200

    *

---
+++ client.js
@@ -0,0 +1,11 @@
+var budget = 100;
+var product = 60;
+
+let result = document.getElementById("result");
+if (product <= budget) {
+    result.innerHTML = "U heeft genoeg geld!"
+    result.style.color = "green";
+} else {
+    result.innerHTML = "Helaas, te weinig geld"
+    result.style.color = "red";
+}
